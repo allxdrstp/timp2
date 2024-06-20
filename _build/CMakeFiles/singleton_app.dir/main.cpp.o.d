ff2efbf478e74b163d98d31c31f944d4ba046132
allxdrstp <145099647+allxdrstp@users.noreply.github.com>
2024-06-20 10:43:58 +0300

    Add files via upload

---
+++ _build/CMakeFiles/singleton_app.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/singleton_app.dir/main.cpp.o: \
+ /home/alexandra/allxdrstp/workspace/zadanie/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/alexandra/allxdrstp/workspace/zadanie/class.h
