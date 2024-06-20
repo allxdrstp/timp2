ea1ee96b248e8671fea422bdabc80f56e645c914
allxdrstp <145099647+allxdrstp@users.noreply.github.com>
2024-06-20 10:51:26 +0300

    Delete _build directory

--- _build/CMakeFiles/singleton_app.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/singleton_app.dir/main.cpp.o: \
- /home/alexandra/allxdrstp/workspace/zadanie/main.cpp \
- /usr/include/stdc-predef.h \
- /home/alexandra/allxdrstp/workspace/zadanie/class.h
